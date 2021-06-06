b40964d025f4621b5bf9ba5af5e093ddbc71533d
Michael Romay <amerror@gmail.com>
2021-06-06 15:00:53 -0700

    added keycode listener for enter key, created logic for arrays to get populated each time enter or send is hit

--- src/components/chatWindow/ChatWindow.jsx
+++ src/components/chatWindow/ChatWindow.jsx
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import useEventListener from '@use-it/event-listener';
 import './chatWindow.css';
+const axios = require('axios');
+
+
+// const textGrabber = (e) => {
+//   console.log('state will be put in here', e);
+// };
+
+let messageArray = [];
 
 const ChatWindow = () => {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  useEventListener('keydown', (e) => {
+    if (e.keyCode === 13) {
+      console.log('enter hit');
+      handleSendMessage();
+    }
+  });
+
+  const handleChange = async (e) => {
+    e.preventDefault();
+    setMessage(e.target.value);
+    // console.log(message);
+  };
+
+  const getCall = () => {
+    axios.get('https://thing3.hosted-models.runwayml.cloud/v1/info')
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  };
+
+  const handleSendMessage = async () => {
+    getCall();
+    await messageArray.push(message);
+    await setMessages(messageArray);
+    await console.log('message: ', message);
+    await console.log('messages: ', messages);
+    // console.log('messageArray: ', messageArray);
+  };
+
+  useEffect(() => {
+    const doSomething = () => {
+      if (window.keyCode === 13) {
+        // Cancel the default action, if needed
+        window.preventDefault();
+        // Trigger the button element with a click
+        handleSendMessage();
+      }
+    };
+
+    window.addEventListener('keyup', doSomething);
+    return () => {
+      window.removeEventListener('keyup', doSomething);
+    };
+  }, []);
+  
   return (
     <section className="chatWindow">
       <section style={{ 
@@ -18,7 +83,16 @@ const ChatWindow = () => {
           </section>
         </section>
         <section className="window-body">
-          <textarea className="textArea"
+  
+        Message:
+          <input
+            type='text'
+            value={message}
+            onChange={handleChange}
+          />
+          <button onClick={handleSendMessage}>send</button>
+
+          {/* <textarea className="textArea"
             style={{ 
               width: 150, 
               height: 50,
@@ -26,17 +100,9 @@ const ChatWindow = () => {
               marginTop: 10,
             }}>
             chat log here
-          </textarea>
-          <textarea
-            style={{ 
-              width: 150, 
-              height: 50,
-              marginLeft: 10,
-              marginTop: 10,
-            }}>
-            type here
-          </textarea>
-          <button>send</button>
+          </textarea> */}
+          
+        
           {/* <p>Hi Mikey 🙂✨</p> */}
         </section>
       </section>
