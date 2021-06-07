14152e0de518e0edc534311513d0c3bc4efe9978
Michael Romay <amerror@gmail.com>
2021-06-06 19:53:14 -0700

    things are working now

--- src/components/chatWindow/ChatWindow.jsx
+++ src/components/chatWindow/ChatWindow.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import useEventListener from '@use-it/event-listener';
 import './chatWindow.css';
 
-let messageArray = [];
 let sendAudio = new Audio('/mp3/send.mp3');
 let receiveAudio = new Audio('/mp3/receive.mp3');
 let loginAudio = new Audio('/mp3/login.mp3');
@@ -11,34 +10,17 @@ const axios = require('axios');
 const ChatWindow = () => {
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const [generatedMessage, setGeneratedMessage] = useState('');
-
-  // const getCall = () => {
-  //   axios.get('https://thing3.hosted-models.runwayml.cloud/v1/info')
-  //     .then(function (response) {
-  //       // handle success
-  //       console.log(response);
-  //     })
-  //     .catch(function (error) {
-  //       // handle error
-  //       console.log(error);
-  //     })
-  //     .then(function () {
-  //       // always executed
-  //     });
-  // };
 
   useEffect(() => {
     loginAudio.play();
   }, []);
 
   const postCall = (passedInMessage) => {
-    axios.post('https://thing3.hosted-models.runwayml.cloud/v1/query', {
-      prompt: passedInMessage,
-    })
-      .then(async function (response) {
-        // await response;
-        // await console.log(response);
+    axios
+      .post('https://thing3.hosted-models.runwayml.cloud/v1/query', {
+        prompt: passedInMessage,
+      })
+      .then(function (response) {
         console.log('postResponse: ', response.data.generated_text);
         handleReceiveMessage(response.data.generated_text);
       })
@@ -50,38 +32,31 @@ const ChatWindow = () => {
   const handleChange = async (e) => {
     e.preventDefault();
     setMessage(e.target.value);
-    // console.log(message);
   };
 
   const handleReceiveMessage = async (passedInMessage) => {
-    // await setGeneratedMessage(passedInMessage);
-    // await messageArray.push('LpCpUnK: ' + generatedMessage);
-    // await console.log('generatedMessage: ', generatedMessage);
-    // await setMessages(messageArray);
-    setTimeout(async function(){ 
-      await setGeneratedMessage(passedInMessage);
-      await messageArray.push('LpCpUnK: ' + generatedMessage);
-      await console.log('generatedMessage: ', generatedMessage);
-      await setMessages(messageArray);
+    setTimeout(async function () {
+      await console.log('generatedMessage: ', passedInMessage);
+      await setMessages((oldState) => [
+        ...oldState,
+        'LpCpUnK: ' + passedInMessage,
+      ]);
       receiveAudio.play();
-    }, 3000);
+    }, 2000);
   };
 
   const handleSendMessage = async () => {
-    // getCall();
-    sendAudio.play();
-    await messageArray.push('immausername: ' + message);
-    await setMessages(messageArray);
+    await setMessages((oldState) => [...oldState, 'immausername: ' + message]);
     await console.log('message: ', message);
     await console.log('messages: ', messages);
     await postCall(message);
     await setMessage('');
+    sendAudio.play();
     // console.log('messageArray: ', messageArray);
   };
 
-  const messageNodes = messageArray.map((message) => {
-    // console.log(message + messageArray.length);
-    return <p key={message + Date.now() + Math.random()}>{message}</p>;
+  const messageNodes = messages.map((message, idx) => {
+    return <p key={message + idx}>{message}</p>;
   });
 
   useEventListener('keydown', (e) => {
@@ -93,43 +68,24 @@ const ChatWindow = () => {
 
   return (
     <section className="chatWindow">
-      <section style={{ 
-        width: 300, 
-        verticalAlign: 'center', 
-        marginTop: '25vh', 
-        marginLeft: '50vh',
-        position: 'unset',
-      }} 
-      className="window">
+      <section
+        style={{
+          width: 300,
+          verticalAlign: 'center',
+          marginTop: '25vh',
+          marginLeft: '50vh',
+          position: 'unset',
+        }}
+        className="window"
+      >
         <section className="title-bar">
-          <section className="title-bar-text">
-          mikeyBot
-          </section>
+          <section className="title-bar-text">mikeyBot</section>
         </section>
         <section className="window-body">
-          <section>
-            {messageNodes}
-          </section>
-        Message:
-          <input
-            type='text'
-            value={message}
-            onChange={handleChange}
-          />
+          <section>{messageNodes}</section>
+          Message:
+          <input type="text" value={message} onChange={handleChange} />
           <button onClick={handleSendMessage}>send</button>
-
-          {/* <textarea className="textArea"
-            style={{ 
-              width: 150, 
-              height: 50,
-              marginLeft: 10,
-              marginTop: 10,
-            }}>
-            chat log here
-          </textarea> */}
-          
-        
-          {/* <p>Hi Mikey 🙂✨</p> */}
         </section>
       </section>
     </section>
